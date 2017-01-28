c8d786ec0cf60a646986f28ef8c5a3dd42f7497d
Paramesh <parameswaran@czsm.co.in>
2017-01-28 12:43:05 +0530

    latest changes

--- firecrack/ViewController.swift
+++ firecrack/ViewController.swift
@@ -16,6 +16,9 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
   
     var player:AVAudioPlayer = AVAudioPlayer()
    
+    @IBOutlet weak var viewUpdown: UIView!
+    
+    
     @IBOutlet weak var brightness: UILabel!
    
     @IBOutlet weak var popView: UIView!
@@ -27,7 +30,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
 
    
     @IBOutlet weak var slidervalue: UILabel!
-    var mygifNames = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60"]
+    var mygifNames = [ "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60"]
     
     var mygif = [UIImage]()
     
@@ -39,7 +42,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
         
 //        UIScreen.main.brightness = CGFloat(0.5)
         
-        
+        self.viewUpdown.isHidden = true
         
         popView.isHidden = true
         popView1.isHidden = true
@@ -73,10 +76,26 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
         player.play()
         
         
-        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(sender:)))
+        myImages.addGestureRecognizer(tapGesture)
         
         
     }
+    
+    func handleTap(sender: UITapGestureRecognizer) {
+        if self.viewUpdown.isHidden{
+          self.viewUpdown.isHidden = false
+            
+        }
+        else{
+            self.viewUpdown.isHidden = true
+            
+        }
+        
+        
+    }
+    
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
@@ -113,7 +132,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
                 
             }
         
-        
+       popView.layer.cornerRadius = 15
     }
   
        
@@ -138,6 +157,8 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
             popView1.isHidden = true
             
         }
+   
+     popView1.layer.cornerRadius = 15
     }
     
     
@@ -153,8 +174,11 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
         
     }
     
+    
+   
+    
+    
 }
-
 
 
 
